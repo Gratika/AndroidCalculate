849d741a16fecbbc072aed6d3307675f15c43b5a
gratika <gratika1@gmail.com>
2023-06-19 22:02:24 +0300

    Створила інтерфейс

---
+++ app/src/main/java/com/gatikahome/androidhw1/CalcActivity.java
@@ -0,0 +1,14 @@
+package com.gatikahome.androidhw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
